3e7e273bf79c6f89f1424dd84c28617d82fe25ec
kevinqiao <qiaoqs@gmail.com>
2024-01-26 20:24:48 -0500

    save

--- src/App.js
+++ src/App.js
@@ -21,7 +21,10 @@ app.post("/telegram-auth", (req, res) => {
   const dataCheckString = validData.initDataStr;
   console.log(validData);
   const strs = dataCheckString.split("&");
-  const sdata = strs.sort().join("\n");
+  const sdata = strs
+    .filter((s) => s.indexOf("hash") < 0)
+    .sort()
+    .join("\n");
   console.log(sdata);
 
   //   // 检查哈希是否存在
