338ebe4354ac33f6b0e9fffc7ededda39d827859
kevinqiao <qiaoqs@gmail.com>
2024-01-26 19:47:05 -0500

    save

--- src/App.js
+++ src/App.js
@@ -17,47 +17,45 @@ app.get("/test", (req, res) => {
 });
 app.post("/telegram-auth", (req, res) => {
   console.log("telegram-auth");
-  const userData = req.body;
+  const validData = req.body;
+  const dataCheckString = validData.initDataStr;
 
-  // 检查哈希是否存在
-  if (!userData.hash) {
-    return res.status(400).send("缺少哈希值");
-  }
+  //   // 检查哈希是否存在
+  //   if (!userData.hash) {
+  //     return res.status(400).send("缺少哈希值");
+  //   }
 
-  // 生成数据检查字符串
-  const dataCheckString = Object.keys(userData)
-    .filter((key) => key !== "hash")
-    .sort()
-    .map((key) => `${key}=${userData[key]}`)
-    // .map((key) => {
-    //   const value = userData[key];
-    //   return `${key}=${typeof value === "object" ? JSON.stringify(value) : value}`;
-    // })
-    .join("\n");
-  console.log(dataCheckString);
-  const lines = dataCheckString.split("\n");
-  console.log(lines);
-  const keyValueRegex = /^[a-zA-Z_]+=[^\s]+$/; // 简单的正则表达式示例
+  //   // 生成数据检查字符串
+  //   const dataCheckString = Object.keys(userData)
+  //     .filter((key) => key !== "hash")
+  //     .sort()
+  //     .map((key) => `${key}=${userData[key]}`)
+  //     // .map((key) => {
+  //     //   const value = userData[key];
+  //     //   return `${key}=${typeof value === "object" ? JSON.stringify(value) : value}`;
+  //     // })
+  //     .join("\n");
+  //   console.log(dataCheckString);
+  //   const lines = dataCheckString.split("\n");
+  //   console.log(lines);
+  //   const keyValueRegex = /^[a-zA-Z_]+=[^\s]+$/; // 简单的正则表达式示例
 
-  for (const line of lines) {
-    if (!keyValueRegex.test(line)) {
-      console.error(`格式错误的行: ${line}`);
-    }
-  }
+  //   for (const line of lines) {
+  //     if (!keyValueRegex.test(line)) {
+  //       console.error(`格式错误的行: ${line}`);
+  //     }
+  //   }
 
   // 使用 Bot Token 生成哈希值的密钥
-  const secretKey = crypto
-    .createHash("sha256")
-    .update(TELEGRAM_BOT_TOKEN + "WebAppData")
-    .digest();
+  const secretKey = crypto.createHash("sha256", "WebAppData").update(TELEGRAM_BOT_TOKEN).digest();
 
   // 使用密钥生成哈希值
   const hash = crypto.createHmac("sha256", secretKey).update(dataCheckString).digest("hex");
-  console.log(userData.hash + ":" + hash);
+  console.log(validData.initDataObj.hash + ":" + hash);
   // 比较哈希值
-  if (hash === userData.hash) {
+  if (hash === validData.initDataObj.hash) {
     // 验证成功
-    res.send({ status: "success", data: userData });
+    res.send({ status: "success", data: validData });
   } else {
     // 验证失败
     res.status(401).send({ status: "error", message: "数据验证失败" });
